10adea549269b96b1a025ebe9f27dbdade15dd2b
mayconghizoni <maycon_ghizoni@hotmail.com>
2019-10-11 21:40:03 -0300

    importação inicial front-end

---
+++ js/site.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    MERCADINHO.produto.validaCadastro() = function(){
+
+        var produto = new Object()
+        produto.name = document.frmCadastroProduto.nomeProduto
+        produto.valorProduto = document.frmCadastroProduto.valorProduto
+
+
+    }
+
+
+
+
+})
