3b0f38ce09e5d5003b4f90fe42540fbdf91c42f6
mayconghizoni <maycon_ghizoni@hotmail.com>
2019-10-14 19:32:47 -0300

    insert marcas

--- system/cadastroMarcas.php
+++ system/cadastroMarcas.php
@@ -13,33 +13,22 @@ else{
     $taxa = $_POST["taxaImposto"];
 
     try {
-        $db_connec = new PDO("pgsql:host=localhost; dbname=mercadinho;", "postgres", "postgres");
 
-        var_dump($db_connec);
+        $pdo = new PDO("pgsql:host=localhost; dbname=mercadinho;", "postgres", "postgres");
 
-        var_dump($db_connec->query("insert into marcas ('nome','taxa_imposto') values ('fini', 10)"));
+        $pdo_exec = $pdo->exec("INSERT INTO marca (id, nome, taxa_imposto) VALUES (nextval('books_sequence'),'$nome', $taxa);");
 
-
-        // $query ="select * from marcas;";
-
-        // if($db_connec){
-        //     echo ("Sucesso!");
-        // }
-
-        // $stm = $db_connec->prepare($query);   
-        // $stm->execute(); 
-
+        if($pdo_exec){
+            echo  "<script> alert('Cadastro efetuado com sucesso!');";
+            echo "javascript:window.location='../pages/cadastros/cadastroMarcas.html';</script>";
+        }else{
+            echo "<script> alert('Erro ao efetuar cadastro!');";
+            echo "javascript:window.location='../pages/cadastros/cadastroMarcas.html';</script>";
+        }
 
      } catch (PDOException  $e) {
         print $e->getMessage();
      }
-
-    // $connec = pg_connect("host=localhost port=5432 dbname=mercadinho user=postgres password=postgres");
-    // $resul = pg_query($connec, "INSERT INTO marcas (nome, taxa_imposto) VALUES ('Fini', 0.90);");
-    //     if  (!$result) {
-    //         echo "<script>alert('Erro ao inserir dados no banco.');";
-    //         echo "javascript:window.location='../pages/cadastros/cadastroMarcas.html';</script>";
-    //     }
 
 }   
 
