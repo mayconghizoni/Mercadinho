d651badb0bc77f116160106be650de7f4d2d7da6
mayconghizoni <maycon_ghizoni@hotmail.com>
2019-10-26 20:45:24 -0300

    2019-10-26 vendas geral

---
+++ pages/vendas.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles/site.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Mercadinho da Soft</title>
+</head>
+<body>
+    <header>
+        <h1>Mercadinho da Soft - Venda de produtos</h1>
+
+        <nav>
+            <a href="/MercadinhoDaSoft/index.html" type="button" class="btn btn-secondary btn-sm btn-sm">Home</a>
+            <a href="/MercadinhoDaSoft/pages/produtos.php" type="button" class="btn btn-secondary btn-sm btn-sm">Produtos</a>
+            <a href="/MercadinhoDaSoft/pages/impostos.php" type="button" class="btn btn-secondary btn-sm btn-sm">Impostos</a>
+            <a href="/MercadinhoDaSoft/pages/vendas.php" type="button" class="btn btn-secondary btn-sm btn-sm">Vendas</a>
+        </nav>
+    </header>
+
+    <section>
+        
+        <form name="frmVendaProduto" id="frmVendaProduto" method="POST" action="../system/venda_produto.php">
+
+            <!-- Deve puxar produtos cadastrados no banco de dados -->
+            <legend>Selecione um produto para venda</legend>
+
+            <div class="form-row align-items-center">
+                <div class="col-auto my-1">
+                <label for="selecionarProduto">Selecione o produto: </label>
+                  <select name="selecionarProduto" class="custom-select mr-sm-2" id="selecionarProduto">
+
+                  <option value="">Escolha</option>
+
+                    <?php
+
+                        //importa arquivo de conexao com banco de dados
+                        include "../system/connection/conexao.php";
+
+                        //Carrega produtos do banco de dados criando uma tag html <option> para cada item do banco, passsando o id dos produtos como valor da option
+                        foreach($pdo->query('select * from produtos') as $row){
+                            echo '<option value="'.$row['id'].'">'.$row['nome'].'</option>';
+                        }   
+                        
+                    ?>
+
+                  </select>
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label for="qtdVendida">Quantos produtos deseja vender: </label>
+                <input name="qtdVendida" type="text" class="form-control" id="qtdVendida" placeholder="2">
+            </div>
+
+            <hr>
+            
+            <button type="submit" class="btn btn-primary">Vender produto</button>
+            <button type="button" class="btn btn-danger">Cancelar</button>
+            
+        </form>
+
+    </section>
+</body>
+</html>
